d4f2e7051253415143e06e3bc70bffa4abbcecc8
guilhermefos <guilhermefos.developer@gmail.com>
2016-11-28 12:34:53 -0200

    uncommented app.php Provider and Aliases

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /**
          * Others Service Providers ...
          */
+         Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -205,6 +206,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
     ],
 
 ];
