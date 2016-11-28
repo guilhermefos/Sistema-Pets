4a1fc9b83521170f4b7389379da1100e5e6d6e8c
guilhermefos <guilhermefos.developer@gmail.com>
2016-11-28 12:31:29 -0200

    Delete CollectiveHtmlHtmlFacade

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         /**
          * Others Service Providers ...
          */
-         //Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -206,9 +205,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        //'Form' => Collective\Html\FormFacade::class,
-        //'Html' => Collective\Html\HtmlFacade::class,
-
     ],
 
 ];
